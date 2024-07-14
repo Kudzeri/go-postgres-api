68078cd4bca680be8ec207527771d68f5ad700aa
Kudzeri <kudzeri@gmail.com>
2024-07-14 15:04:41 +0500

    init server

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/Kudzeri/go-postgres-api/router"
+	"log"
+	"net/http"
+)
 
+func main() {
+	r := router.Router()
+	fmt.Println("Starting server on the port 8080...")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
