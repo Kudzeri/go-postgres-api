cffcadd8957be3cb2ff455c4cf2fe06c0d0333ea
Kudzeri <kudzeri@gmail.com>
2024-07-14 15:01:03 +0500

    init project

--- router/router.go
+++ router/router.go
@@ -1,14 +1,15 @@
 package router
 
 import "github.com/gorilla/mux"
+import "github.com/Kudzeri/go-postgres-api/middleware"
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/stocks/{id}", middleware.GetStock).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/stocks", middleware.GetAllStocks).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/stocks/new", middleware.CreateStock).Methods("POST", "OPTIONS")
-	router.HandleFunc("/", middleware.UpdateStock).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/api/stocks/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/newstock", middleware.CreateStock).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/updatestock", middleware.UpdateStock).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deletestock/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
 
 }
