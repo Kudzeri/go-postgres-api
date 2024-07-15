c5803fe3867f58ede34e40630a248620e6ad11aa
Kudzeri <kudzeri@gmail.com>
2024-07-15 13:01:52 +0500

    add env connection function

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Kudzeri/go-postgres-api
 
 go 1.22.4
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+)
--- middleware/handlers.go
+++ middleware/handlers.go
@@ -1,8 +1,19 @@
 package middleware
 
+import (
+	"database/sql"
+	"log"
+)
+import "github.com/joho/godotenv"
+
 type Response struct {
 	ID      int64  `json:"id,omitempty"`
 	Message string `json:"message,omitempty"`
 }
 
-func CreateConnection()
+func CreateConnection() *sql.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("handlers.CreateConnection():Can't upload the env file")
+	}
+}
